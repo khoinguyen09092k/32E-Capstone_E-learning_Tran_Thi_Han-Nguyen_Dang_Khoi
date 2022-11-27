403751d71e0e510f19e7136e00f0a060b7fff08c
khoinguyen09092k <nguyendangkhoi0909@gmail.com>
2022-11-28 00:21:50 +0700

    uppp

--- src/templates/Home/Layouts/footer.js
+++ src/templates/Home/Layouts/footer.js
@@ -26,64 +26,22 @@ export default function Footer() {
 
   return (
     <Fragment>
-      {/* <div className="container">
-        <LazyLoad>
-          <div
-            className="py-10 sm:py-16 px-5 mt-10 sm:mt-0 sm:px-0 mb-12 sm:mb-24 mx-5 sm:mx-0 flex justify-center rounded-3xl"
-            style={{
-              backgroundImage: 'url("/images/bgNewsletter.png")',
-              backgroundPosition: "center center",
-              backgroundSize: "1400px auto",
-              backgroundRepeat: "no-repeat",
-              backgroundColor: "#0A033C",
-            }}
-          >
-            <div className="text-center">
-              <h2 className="text-xl sm:text-5xl text-white font-semibold leading-tight">
-                Subscribe For Get Update
-                <br />
-                Every New Courses
-              </h2>
-              <p className="text-sm sm:text-xl text-gray-400 leading-normal">
-                20k+ students daily learn with Cybersoft. Subscribe for new courses.
-              </p>
-              <form>
-              <div className="mt-7 sm:mt-24 flex flex-row">
-                <input
-                  type="email"
-                  placeholder="example@email.com"
-                  className="w-3/5 px-3 py-2 lg:p-3 rounded-l-lg sm:w-2/3"
-                />
-                <button
-                  type="submit"
-                  className="bg-purple-600 hover:bg-purple-700 text-white font-semibold text-base p-5 rounded-r-lg transition-colors
-                w-2/5 sm:w-1/3 dark:bg-violet-400 dark:text-coolGray-900"
-                >
-                  Subscribe
-                </button>
-              </div>
-              </form>
-            </div>
-          </div>
-        </LazyLoad>
-      </div> */}
       <footer className="px-4 divide-y dark:bg-coolGray-800 dark:text-coolGray-100 ">
         <div className="footer container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
-          <div className="lg:w-1/3">
+          <div className="lg:w-1/4">
             <a
               href="/"
               className="flex  justify-start sm:justify-center space-x-3 lg:justify-start"
             >
               <div className="flex items-center justify-start sm:justify-center rounded-full dark:bg-violet-400">
-                <img className="w-3/4 sm:w-4/5"src="/images/logo.png" alt="E - LEARNING"/>
-                {/* <h1 style={{fontSize:30}}>E - LEARNING</h1> */}
+                <img className="w-2/4 sm:w-3/5"src="/images/logo.png" alt="E - LEARNING"/>
               </div>
             </a>
           </div>
           <div className="footer_links grid grid-cols-2 text-lg gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
             <div className="footer_links_course space-y-3">
               <h3 className="font-bold text-lg sm:text-2xl tracking-wide uppercase dark:text-coolGray-50">
-                Khoá học
+               Môn Học
               </h3>
               <ul className="space-y-1">
                 {renderCateCourses()}
@@ -91,58 +49,55 @@ export default function Footer() {
             </div>
             <div className="footer_links_community space-y-3">
               <h3 className="font-bold  text-lg sm:text-2xl tracking-wide uppercase dark:text-coolGray-50">
-                Community
+               LIÊN KẾT 
               </h3>
               <ul className="space-y-1 text-base sm:text-lg">
                 <li>
-                  <a href="#~">Learners</a>
+                  <a href="#~">Trang Chủ</a>
                 </li>
                 <li>
-                  <a href="#~">Parteners</a>
+                  <a href="#~">Dịch Vụ</a>
                 </li>
                 <li>
-                  <a href="#~">Developers</a>
+                  <a href="#~">Hội Nhóm</a>
                 </li>
                 <li>
-                  <a href="#~">Transactions</a>
+                  <a href="#~">Giảng Viên</a>
                 </li>
                 <li>
                   <a href="#~">Blog</a>
-                </li>
-                <li>
-                  <a href="#~">Teaching Center</a>
                 </li>
               </ul>
             </div>
             <div className="footer_links_quick space-y-3">
               <h3 className="font-bold  text-lg sm:text-2xl uppercase dark:text-coolGray-50">
-                Quick links
+                NHÀ TÀI TRỢ
               </h3>
               <ul className="space-y-1 text-base sm:text-lg">
                 <li>
-                  <a href="#~">Professional Education</a>
+                  <a href="#~">FPT - SHOP</a>
                 </li>
                 <li>
-                  <a href="#~">Courses</a>
+                  <a href="#~">DXC VIETNAM</a>
                 </li>
                 <li>
-                  <a href="#~">Admissions</a>
+                  <a href="#~">BOSH</a>
                 </li>
                 <li>
-                  <a href="#~">Testimonial</a>
+                  <a href="#~">TMA SOLUTIONS</a>
                 </li>
                 <li>
-                  <a href="#~">Programs</a>
+                  <a href="#~">SIC VIETNAM</a>
                 </li>
               </ul>
             </div>
             <div className="space-y-3">
               <div className="font-bold  text-lg sm:text-2xl uppercase dark:text-coolGray-50">
-                Mạng xã hội
+                LIÊN HỆ TƯ VẤN
               </div>
               <div className="flex justify-start space-x-2">
                 <a
-                  href="https://www.facebook.com/truc.ngan.00"
+                  href=""
                   title="Facebook"
                   className="flex items-center p-1"
                 >
@@ -156,7 +111,7 @@ export default function Footer() {
                   </svg>
                 </a>
                 <a
-                  href="https://www.instagram.com/tnagn__/"
+                  href=""
                   title="Instagram"
                   className="flex items-center p-1"
                 >
@@ -170,7 +125,7 @@ export default function Footer() {
                   </svg>
                 </a>
                 <a
-                  href="https://github.com/trucngan1204"
+                  href=""
                   title="Github"
                   className="flex items-center p-1"
                 >
@@ -183,27 +138,13 @@ export default function Footer() {
                     <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z" />
                   </svg>
                 </a>
-                {/* <a
-                  href="https://www.behance.net/royalthai"
-                  title="Behance"
-                  className="flex items-center p-1"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.228 15.01h-2.228v-2.01h2.261c1.878 0 2.003 2.01-.033 2.01zm6.758-2.677h3.018c-.117-1.715-2.73-1.977-3.018 0zm-6.804-3.333h-2.182v2h2.389c1.673 0 1.937-2-.207-2zm15.818-4v14c0 2.761-2.238 5-5 5h-14c-2.762 0-5-2.239-5-5v-14c0-2.761 2.238-5 5-5h14c2.762 0 5 2.239 5 5zm-10 3h5v-1h-5v1zm-3.552 3.618c1.907-.974 1.837-4.55-1.813-4.604h-4.635v9.978h4.311c4.522 0 4.445-4.534 2.137-5.374zm9.487.602c-.274-1.763-1.528-2.95-3.583-2.95-2.094 0-3.352 1.34-3.352 3.947 0 2.631 1.367 3.783 3.416 3.783s3.106-1.135 3.4-2h-2.111c-.736.855-2.893.521-2.767-1.353h5.06c.01-.634-.012-1.089-.063-1.427z" />
-                  </svg>
-                </a> */}
               </div>
             </div>
           </div>
         </div>
         <div className="footer_copyright py-6 text-sm text-center dark:text-coolGray-400">
-          © 2022 FE71 -
-          <a href="#"> Trúc Ngân Neeee</a>
+          © 2022 E-LEARNING - 
+          <a href="#"> TTH & NDK</a>
         </div>
       </footer>
     </Fragment>
--- src/templates/Home/Pages/HomeContent.js
+++ src/templates/Home/Pages/HomeContent.js
@@ -5,50 +5,29 @@ import CarouselHome from "../Components/Home/CarouselHome";
 import TabCoursesHome from "../Components/Home/tabCoursesHome";
 import { history } from "../../../App";
 import { Link, NavLink } from "react-router-dom";
-<<<<<<< HEAD
-
-=======
 import img from "./../../../../src/assets/images/img7.jpg"
 import face1 from "./../../../../src/assets/images/face1.jpg"
 import face2 from "./../../../../src/assets/images/face2.jpg"
->>>>>>> main
 export default function HomeContent(props) {
   return (
     <Fragment>
       <CarouselHome />
-<<<<<<< HEAD
-      <div className="container mx-auto py-5 sm:py-16">
-        <div className="heading text-center py-10 sm:py-14">
-          <h1 className="text-3xl px-10 sm:text-6xl font-bold py-3">
-          Đào tạo nghề lập trình chuyên sâu từ ZERO
-          </h1>
-          <p className="text-base font-normal px-2 sm:px-80">
-          CyberSoft khai thác nhu cầu tuyển dụng lập trình, 
-          kết nối việc làm tới doanh nghiệp và tích hợp 
-          các dự án với công nghệ mới nhất vào phương pháp 
-          đào tạo tích cực cho các học viên học xong có việc làm ngay. 
-          Chương trình giảng dạy được biên soạn may đo dành riêng 
-          cho các bạn Lập trình từ trái ngành hoặc đã có kiến thức theo cường độ cao, 
-          luôn được tinh chỉnh và tối ưu hóa theo thời gian bởi các thành viên sáng lập và giảng viên dày kinh nghiệm.
-          </p>
-          <button
-=======
       <div className="container mx-auto sm:py-8 mb-8">
-            <div className="text-center">
-              <h2 className="text-4xl sm:text-4xl  py-3">
-                KHÓA HỌC TẠI <span className="text-green-500">VLearning</span>
-              </h2>
-              <p className="text-md text-gray-500 font-normal px-2 mb-5 sm:px-80">
-                Học qua dự án thực tế, học đi đôi với hành, không lý thuyết lan man, phân tích cội nguồn của vấn đề, xây dựng từ các ví dụ nhỏ đến thực thi một dự án lớn ngoài thực tế để học viên học xong làm được ngay. Hơn 1000 bài tập và dự án thực tế</p>
+        <div className="text-center">
+          <h2 className="text-4xl sm:text-4xl  py-3">
+            KHÓA HỌC TẠI <span className="text-green-500">VLearning</span>
+          </h2>
+          <p className="text-md text-gray-500 font-normal px-2 mb-5 sm:px-80">
+            Học qua dự án thực tế, học đi đôi với hành, không lý thuyết lan man, phân tích cội nguồn của vấn đề, xây dựng từ các ví dụ nhỏ đến thực thi một dự án lớn ngoài thực tế để học viên học xong làm được ngay. Hơn 1000 bài tập và dự án thực tế</p>
 
-            </div>
+        </div>
         <div className="grid grid-cols-2">
           <div>
-          <img className="" src={img} alt="" style={{width:'100%'}}/>
-             
+            <img className="" src={img} alt="" style={{ width: '100%' }} />
+
           </div>
-          <div className="" style={{paddingTop:'50px'}}>
-           
+          <div className="" style={{ paddingTop: '50px' }}>
+
             <div className="grid grid-cols-2 gap-4">
               <div >
                 <div className="mb-4">
@@ -58,19 +37,19 @@ export default function HomeContent(props) {
                 </div>
                 <div>
                   <h4 className="text-lg">LỘ TRÌNH PHÙ HỢP</h4>
-                 <p> <i class="fa-solid fa-circle-check text-green-500 mr-3"></i>Lộ trình bài bản từ zero tới chuyên nghiệp, nâng cao</p>
-                 <p> <i class="fa-solid fa-circle-check text-green-500 mr-3"></i>Huấn luyện để phát triển năng lực và niềm đam mê lập trình</p>
-                  
+                  <p> <i class="fa-solid fa-circle-check text-green-500 mr-3"></i>Lộ trình bài bản từ zero tới chuyên nghiệp, nâng cao</p>
+                  <p> <i class="fa-solid fa-circle-check text-green-500 mr-3"></i>Huấn luyện để phát triển năng lực và niềm đam mê lập trình</p>
+
                 </div>
               </div>
 
               <div>
                 <div>
                   <h4 className="text-lg">GIẢNG VIÊN</h4>
-                 <p><i class="fa-solid fa-circle-check text-green-500 mr-3"></i>
-                 Tương tác cùng mentor và giảng viên qua phần thảo luận
-                 </p>
-                 <p> <i class="fa-solid fa-circle-check text-green-500 mr-3"></i>Review code và đưa ra các nhận xét góp ý, tương tác thảo luận giữa các học viên</p>
+                  <p><i class="fa-solid fa-circle-check text-green-500 mr-3"></i>
+                    Tương tác cùng mentor và giảng viên qua phần thảo luận
+                  </p>
+                  <p> <i class="fa-solid fa-circle-check text-green-500 mr-3"></i>Review code và đưa ra các nhận xét góp ý, tương tác thảo luận giữa các học viên</p>
 
                 </div>
                 <div>
@@ -80,49 +59,27 @@ export default function HomeContent(props) {
                 </div>
               </div>
               <button
->>>>>>> main
-            onClick={(e) => {
-              e.preventDefault();
-              history.push("/courses");
-            }}
-<<<<<<< HEAD
-            className="bg-purple-600 hover:bg-purple-700 text-white font-semibold text-base p-5 mt-8 rounded-lg transition-colors"
-          >
-            Xem các khoá học
-          </button>
-        </div>
-        <div className="img_screenshotlive_home relative mt-20 flex justify-center">
-          <img
-            className="w-11/12 sm:w-9/12"
-            src="./images/students.jpg"
-            alt=""
-            width="100%"
-          />
-        </div>
-        <div className="heading text-center py-14">
-          <h1 className="text-3xl sm:text-6xl font-bold py-3">
-            Lộ trình và danh sách các khoá học
-          </h1>
-          <p className="text-lg font-normal px-2 mb-24 sm:px-80">
-=======
-            className="bg-green-500 hover:bg-green-500 text-white font-semibold text-base p-5 mt-8 rounded-lg transition-colors"
-          >
-            Xem các khoá học
-          </button>
+                onClick={(e) => {
+                  e.preventDefault();
+                  history.push("/courses");
+                }}
+                className="bg-green-500 hover:bg-green-500 text-white font-semibold text-base p-5 mt-8 rounded-lg transition-colors"
+              >
+                Xem các khoá học
+              </button>
             </div>
           </div>
         </div>
-        <div className="heading text-center " style={{margin:'50px'}}>
+        <div className="heading text-center " style={{ margin: '50px' }}>
           <h2 className="text-4xl sm:text-4xl  py-3">
             LỘ TRÌNH VÀ DANH SÁCH CÁC <span className="text-green-500">KHOÁ HỌC</span>
           </h2>
           <p className="text-md text-gray-500 font-normal px-2 mb-5 sm:px-80">
->>>>>>> main
-                CyberSoft vạch ra lộ trình học lập trình một cách chi tiết, 
-                bài bản, chuyên nghiệp, chuẩn nghề ĐH Arizona -USA, 
-                100% thực hành theo dự án thực tế tránh lan man, 
-                luyện tập tư duy, logic nền tảng chuyên sâu dễ dàng phát triển lên senior, leader. 
-                Đi đúng lộ trình CyberSoft bạn sẽ tiết kiệm được thời gian, chi phí và có việc làm tốt nhất và bền vững để thăng tiến.
+            CyberSoft vạch ra lộ trình học lập trình một cách chi tiết,
+            bài bản, chuyên nghiệp, chuẩn nghề ĐH Arizona -USA,
+            100% thực hành theo dự án thực tế tránh lan man,
+            luyện tập tư duy, logic nền tảng chuyên sâu dễ dàng phát triển lên senior, leader.
+            Đi đúng lộ trình CyberSoft bạn sẽ tiết kiệm được thời gian, chi phí và có việc làm tốt nhất và bền vững để thăng tiến.
           </p>
           <div className="py-8">
             <Lazyload>
@@ -131,57 +88,8 @@ export default function HomeContent(props) {
           </div>
         </div>
 
-<<<<<<< HEAD
-            </h2>
-            <p className="text-base sm:text-xl mb-12 leading-normal">
-                Hơn 92% trên hơn 5000 học viên học tại CyberSoft học theo lộ trình và có việc làm tốt. 
-                CyberSoft đã triển khai chương trình Học lập trình trực tuyến Online tại CyberLearn 
-                để tạo điều kiện cho các bạn không có điều kiện học trực tiếp tại HCM có thể tham gia.
-            </p>
-            <NavLink
-              to="./signup"
-              className="mt-5 bg-purple-600 hover:bg-purple-400 hover:text-white  text-white font-semibold text-sm sm:text-base p-4 sm:p-5 rounded-lg transition-colors"
-            >
-              Đăng ký ngay
-            </NavLink>
-          </div>
-          <div className="img_collegeLevel_home ">
-            <Lazyload>
-              <img src="./images/online.png" alt="" width="100%" />
-            </Lazyload>
-          </div>
-        </div>
-        <div
-          className="mt-24 sm:mt-44 mx-5 sm:mx-0 grid grid-flow-row sm:grid-flow-col sm:grid-cols-2 gap-4"
-        >
-          <div className="img_collegeLevel_home order-2 sm:order-1 mt-10 sm:mt-0">
-            <Lazyload>
-              <img className="w-11/12 sm:w-11/12" src="./images/img.jpg" alt="" />
-            </Lazyload>
-          </div>
-          <div className="order-1 sm:order-2 px-1 sm:px-12 col-span-1">
-            <h2 className="sm:text-6xl text-3xl text-center sm:text-left font-semibold leading-tight">
-            Thay đổi sự nghiệp 
-              <br />
-              - Học lập trình theo lộ trình chuyên biệt tại
-              <br />CyberSoft
-            </h2>
-            <p className="text-lg text-center sm:text-left sm:text-2xl mb-12 sm:leading-10">
-            Đào tạo cho mọi đối tượng từ người trái ngành, người mới bắt đầu, 
-            sinh viên công nghệ thông tin đến các bạn đã có có nghề lập trình. 
-            Đào tạo ra những lập trình viên tài năng, phát huy tố chất, tư duy lập trình, có định hướng để trở thành những lập trình chuyên nghiệp. Giáo trình được may đo và biên soạn dành riêng cho các bạn học lập trình.
-            </p>
-            <Link
-              to="./contact"
-              className="flex mx-auto sm:mx-0 w-fit mt-5 bg-purple-600 hover:bg-purple-700 hover:text-white 
-              text-white font-semibold text-sm sm:text-base p-4 sm:p-5 rounded-lg transition-colors"
-            >
-              Liên hệ với chúng tôi
-            </Link>
-          </div>
-=======
 
-        <div style={{backgroundColor:'rgb(204 204 204 / 12%)',padding:'50px'}}>
+        <div style={{ backgroundColor: 'rgb(204 204 204 / 12%)', padding: '50px' }}>
           <section class="text-gray-600 body-font">
             <div class="container px-5  mx-auto">
               <h3 class="text-4xl sm:text-4xl mb-12 text-center">ĐÁNH GIÁ TỪ HỌC VIÊN</h3>
@@ -190,7 +98,7 @@ export default function HomeContent(props) {
                   <div class="h-full bg-gray-100 p-8 rounded">
                     <i class="fa-solid fa-quote-right text-4xl"></i>
                     <p class="leading-relaxed mb-6">Chương trình giảng dạy được biên soạn dành riêng cho các bạn Lập trình từ trái ngành hoặc đã có kiến thức theo cường độ cao, luôn được tinh chỉnh và tối ưu hóa theo thời gian bởi các thành viên sáng lập và giảng viên dày kinh nghiệm.Thực sự rất hay và hấp dẫn
-.</p>
+                      .</p>
                     <a class="inline-flex items-center">
 
                       <img className=" rounded-full flex-shrink-0 object-cover object-center" src={face1} alt="" style={{ width: '60px', height: '60px' }} />
@@ -206,7 +114,7 @@ export default function HomeContent(props) {
                   <div class="h-full bg-gray-100 p-8 rounded">
                     <i class="fa-solid fa-quote-right text-4xl"></i>
                     <p class="leading-relaxed mb-6">Chương trình giảng dạy được biên soạn dành riêng cho các bạn Lập trình từ trái ngành hoặc đã có kiến thức theo cường độ cao, luôn được tinh chỉnh và tối ưu hóa theo thời gian bởi các thành viên sáng lập và giảng viên dày kinh nghiệm.Thực sự rất hay và hấp dẫn
-</p>
+                    </p>
                     <a class="inline-flex items-center">
                       <img className=" rounded-full flex-shrink-0 object-cover object-center" src={face2} alt="" style={{ width: '60px', height: '60px' }} />
 
@@ -215,15 +123,14 @@ export default function HomeContent(props) {
                         <span class="text-gray-500 text-sm">DESIGNER</span>
                       </span>
                     </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
->>>>>>> main
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </Fragment>
-   
+
   );
 }
